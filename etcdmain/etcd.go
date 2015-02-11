4c0bcbec36b05ab6a9adb458539f04c3cec9b6a7
Yicheng Qin <qycqycqycqycqyc@gmail.com>
2015-02-11 14:45:55 -0800

    etcdmain: fix symlink when migration

--- etcdmain/etcd.go
+++ etcdmain/etcd.go
@@ -360,11 +360,11 @@ func makeMemberDir(dir string) error {
 			// Link it to the subdir and keep the v1 file at the original
 			// location, so v0.4 etcd can still bootstrap if the upgrade
 			// failed.
-			if err := os.Symlink(path.Join(dir, name), path.Join(membdir, name)); err != nil {
+			if err := os.Symlink(path.Join(dir, name), path.Join("..", name)); err != nil {
 				return err
 			}
 		case v2Files.Contains(name):
-			if err := os.Rename(path.Join(dir, name), path.Join(membdir, name)); err != nil {
+			if err := os.Rename(path.Join(dir, name), path.Join("..", name)); err != nil {
 				return err
 			}
 		}
